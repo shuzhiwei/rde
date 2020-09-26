7fa1b812217707a060f036723d9093528b178a7c
aaa <1065537421@qq.com>
2020-09-26 09:23:36 +0800

    初始化提交

---
+++ run.py
@@ -0,0 +1,57 @@
+import web
+import json
+import requests
+import traceback
+import threading
+import jwt, time
+import casbin
+from tools.sync_policy import syncPolicy
+from logger.logger import logger
+
+urls = {
+    "/powerFactor", "PowerFactor",
+}
+
+app = web.application(urls, globals())
+dom = 'rde'
+obj = 'rde'
+
+class PowerFactor:
+    def POST(self):
+        try:
+            web.header("Access-Control-Allow-Origin", "*")
+            token = web.input().token
+            try:
+                parse_token = jwt.decode(token, 'secret', algorithms='HS256')
+            except Exception as e:
+                logger.error(e)
+                logger.error(traceback.format_exc())
+                return json.dumps({'status': 'fail', 'code': 402, 'message': 'token expired'})
+
+            username = parse_token['username']
+            e = casbin.Enforcer("confs/model.conf", "confs/policy.csv")
+            sub = username
+            act = 'read'
+            if e.enforce(sub, dom, obj, act):
+                trans_code = web.input().trans_code
+                start_time = web.input().start_time
+                end_time = web.input().end_time
+                url = 'https://ginkgo.bjrde.cn/baiducloud-webapi/select_max_power_factor_on_week?trans_code=' + trans_code + \
+                        '&start_time=' + start_time + '&end_time=' + end_time
+                logger.debug(url)
+                con = requests.get(url).text
+                return con
+            else:
+                return json.dumps({'status': 'fail', 'code': 401, 'message': 'unauthorization operation'})
+
+        except Exception as e:
+            logger.error(e)
+            logger.error(traceback.format_exc())
+            return json.dumps({'code': 500, 'message': 'fail'})
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=syncPolicy, args=(dom,))
+    logger.debug('start thread sync policy ...')
+    t1.start()
+    logger.debug('start run app ...')
+    app.run()
